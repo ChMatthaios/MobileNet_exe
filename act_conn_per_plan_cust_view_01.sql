e7b56a9e53421af400a518391731763f5a7b7d2a
Matthaios Ch <34523286+ChMatthaios@users.noreply.github.com>
2023-01-31 16:15:41 +0200

    Update act_conn_per_plan_cust_view_01.sql

--- act_conn_per_plan_cust_view_01.sql
+++ act_conn_per_plan_cust_view_01.sql
@@ -1,42 +1,30 @@
 /*Create a view to create a report with the number of 
   active connections per user program and customer type.
 */
+CREATE OR REPLACE FORCE EDITIONABLE VIEW viewa1 (
+    count_subs,
+    plan_name,
+    type_name
+) AS
+    SELECT
+        COUNT(subscription_id) AS count_subs,
+        splan_name             AS plan_name,
+        ctype_name             AS type_name
+    FROM
+             exe_subscription
+        JOIN exe_subs_pl_relation ON subs_id = subscription_id
+        JOIN exe_subscription_plan ON subs_pl_id = splan_id
+        JOIN exe_customer ON subscription_cid = customer_id
+        JOIN exe_customer_type ON customer_tid = ctype_id
+    WHERE
+        subscription_end_date IS NULL
+    GROUP BY
+        splan_name,
+        ctype_name
+    ORDER BY
+        ctype_name;
+
 SELECT
-	COUNT(SUBSCRIPTION_ID) AS COUNT_SUBS,
-	SPLAN_NAME AS PLAN_NAME,
-	CTYPE_NAME AS TYPE_NAME
+    *
 FROM
-	SUBSCRIPTION
-    JOIN
-        SUBS_PL_RELATION
-        ON
-        SUBS_ID = SUBSCRIPTION_ID
-    JOIN
-        SUBSCRIPTION_PLAN
-        ON
-        SUBS_PL_ID = SPLAN_ID
-    JOIN
-        CUSTOMER
-        ON
-        SUBSCRIPTION_CID = CUSTOMER_ID
-    JOIN
-        CUSTOMER_TYPE
-        ON
-        CUSTOMER_TID = CTYPE_ID
-WHERE
-	SUBSCRIPTION_END_DATE IS NULL
-GROUP BY
-	SPLAN_NAME,
-	CTYPE_NAME
-ORDER BY
-	CTYPE_NAME;
-/*
-COUNT_SUBS PLAN_NAME       TYPE_NAME      
----------- --------------- ---------------
-         1 LARGE           COMPANY        
-         2 MEDIUM          COMPANY        
-         1 EXTRA LARGE     PERSON         
-         2 SMALL           PERSON         
-         1 LARGE           PROFESSIONAL   
-         1 SMALL           PROFESSIONAL  
-*/
+    viewa1;

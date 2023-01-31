3a34c4f602f72ee377449ab8711196c0e5dab03f
Matthaios Ch <34523286+ChMatthaios@users.noreply.github.com>
2023-01-31 16:16:53 +0200

    Update owing_cust_view_02.sql

--- owing_cust_view_02.sql
+++ owing_cust_view_02.sql
@@ -3,31 +3,31 @@
   total outstanding balance and date of oldest 
   outstanding bill.
 */
+
+CREATE OR REPLACE FORCE EDITIONABLE VIEW viewa2 (
+    afm,
+    name,
+    sum,
+    old_date
+) AS
+    SELECT
+        c.customer_afm   AS afm,
+        c.customer_name  AS name,
+        SUM(b.bill_rem)  AS sum,
+        MIN(b.bill_date) AS old_date
+    FROM
+             exe_customer c
+        JOIN exe_subscription s ON s.subscription_cid = c.customer_id
+        JOIN exe_billing      b ON b.bill_sid = s.subscription_id
+    WHERE
+        s.subscription_end_date IS NULL
+    GROUP BY
+        c.customer_afm,
+        c.customer_name
+    ORDER BY
+        c.customer_afm;
+
 SELECT
-    C.CUSTOMER_AFM AS AFM,
-    C.CUSTOMER_NAME AS NAME,
-    SUM(B.BILL_REM) AS SUM,
-    MIN(B.BILL_DATE) AS OLD_DATE
-FROM CUSTOMER C
-    JOIN SUBSCRIPTION S
-        ON S.SUBSCRIPTION_CID = C.CUSTOMER_ID
-    JOIN BILLING B
-        ON B.BILL_SID = S.SUBSCRIPTION_ID
-WHERE
-    S.SUBSCRIPTION_END_DATE IS NULL
-GROUP BY
-    C.CUSTOMER_AFM,
-    C.CUSTOMER_NAME
-ORDER BY
-    C.CUSTOMER_AFM;
-/*
-AFM       NAME		SUM			OLD_DATE          
---------- -------	----------	------------------
-123456789 ALFA		30			01-DEC-22 00.00.00
-345678912 GAMMA		100			01-DEC-22 00.00.00
-567891234 EPSILON	35			01-DEC-22 00.00.00
-678912345 ZETA		131			01-DEC-22 00.00.00
-789123456 ITTA		100			01-DEC-22 00.00.00
-891234567 THETA		140			01-DEC-22 00.00.00
-912345678 GIWTA		30			01-DEC-22 00.00.00
-*/
+    *
+FROM
+    viewa2;

c6ddaa6efc555c40df8c6f5089602c035eb497d7
Matthaios Ch <34523286+ChMatthaios@users.noreply.github.com>
2023-05-17 14:09:36 +0300

    Update runnable.sql

--- runnable.sql
+++ runnable.sql
@@ -1,67 +1,52 @@
+/* Formatted on 17/05/2023 14:09:17 (QP5 v5.336) */
 BEGIN
     mobilenet.auto_billing;
-    dbms_output.put_line('AUTO BILLING COMPLETED.');
+    DBMS_OUTPUT.put_line ('AUTO BILLING COMPLETED.');
 END;
 
 BEGIN
-    FOR i IN (
-        SELECT
-            afm
-        FROM
-            viewa2
-    ) LOOP
-        dbms_output.put_line(
-                            CASE mobilenet.balance_search(i.afm)
-                                WHEN 0 THEN
-                                    'Nothing to pay for <'
-                                    || i.afm
-                                    || '>'
-                                ELSE '<'
-                                     || i.afm
-                                     || '>	MUST PAY : '
-                                     || mobilenet.balance_search(i.afm)
-                            END
-        );
+    FOR i IN (SELECT afm FROM viewa2)
+    LOOP
+        DBMS_OUTPUT.put_line (
+            CASE mobilenet.balance_search (i.afm)
+                WHEN 0
+                THEN
+                    'Nothing to pay for <' || i.afm || '>'
+                ELSE
+                       '<'
+                    || i.afm
+                    || '>	MUST PAY : '
+                    || mobilenet.balance_search (i.afm)
+            END);
     END LOOP;
 
-    dbms_output.put_line('BALANCE SEARCH COMPLETED.');
+    DBMS_OUTPUT.put_line ('BALANCE SEARCH COMPLETED.');
 END;
 
 DECLARE
-    l_num NUMBER(3, 0);
+    l_num          NUMBER (3, 0);
 BEGIN
-    FOR i IN (
-        SELECT
-            afm
-        FROM
-            viewa2
-    ) LOOP
-        l_num := dbms_random.value(0, 50);
-        dbms_output.put_line(
-                            CASE mobilenet.balance_search(i.afm)
-                                WHEN 0 THEN
-                                    'Nothing to pay for <'
-                                    || i.afm
-                                    || '>'
-                                ELSE '<'
-                                     || i.afm
-                                     || '>, '
-                                     || l_num
-                            END
-        );
+    FOR i IN (SELECT afm FROM viewa2)
+    LOOP
+        l_num := DBMS_RANDOM.VALUE (0, 50);
+        DBMS_OUTPUT.put_line (
+            CASE mobilenet.balance_search (i.afm)
+                WHEN 0 THEN 'Nothing to pay for <' || i.afm || '>'
+                ELSE '<' || i.afm || '>, ' || l_num
+            END);
 
-        IF mobilenet.balance_search(i.afm) <> 0 THEN
-            mobilenet.payment(i.afm, l_num);
-            dbms_output.put_line('Your balance is : '
-                                 || mobilenet.balance_search(i.afm));
+        IF mobilenet.balance_search (i.afm) <> 0
+        THEN
+            mobilenet.payment (i.afm, l_num);
+            DBMS_OUTPUT.put_line (
+                'Your balance is : ' || mobilenet.balance_search (i.afm));
         END IF;
-
     END LOOP;
 
-    dbms_output.put_line('PAYMENT COMPLETED.');
+    DBMS_OUTPUT.put_line ('PAYMENT COMPLETED.');
 END;
 
 BEGIN
     mobilenet.reset_values;
-    dbms_output.put_line('RESET COMPLETED.');
+    DBMS_OUTPUT.put_line ('RESET COMPLETED.');
 END;

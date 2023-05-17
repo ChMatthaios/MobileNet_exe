1377d72e1206ccd2f22976d332c211a7dd42dd85
Matthaios Ch <34523286+ChMatthaios@users.noreply.github.com>
2023-05-17 14:09:08 +0300

    Update owing_cust_view_02.sql

--- owing_cust_view_02.sql
+++ owing_cust_view_02.sql
@@ -4,30 +4,23 @@
   outstanding bill.
 */
 
-CREATE OR REPLACE FORCE EDITIONABLE VIEW owing_cust_view_02 (
+CREATE OR REPLACE FORCE EDITIONABLE VIEW owing_cust_view_02
+(
     afm,
     name,
-    sum,
+    SUM,
     old_date
-) AS
-    SELECT
-        c.customer_afm   AS afm,
-        c.customer_name  AS name,
-        SUM(b.bill_rem)  AS sum,
-        MIN(b.bill_date) AS old_date
-    FROM
-             exe_customer c
-        JOIN exe_subscription s ON s.subscription_cid = c.customer_id
-        JOIN exe_billing      b ON b.bill_sid = s.subscription_id
-    WHERE
-        s.subscription_end_date IS NULL
-    GROUP BY
-        c.customer_afm,
-        c.customer_name
-    ORDER BY
-        c.customer_afm;
+)
+AS
+    SELECT   c.customer_afm        AS afm,
+             c.customer_name       AS name,
+             SUM (b.bill_rem)      AS SUM,
+             MIN (b.bill_date)     AS old_date
+        FROM exe_customer c
+             JOIN exe_subscription s ON s.subscription_cid = c.customer_id
+             JOIN exe_billing b ON b.bill_sid = s.subscription_id
+       WHERE s.subscription_end_date IS NULL
+    GROUP BY c.customer_afm, c.customer_name
+    ORDER BY c.customer_afm;
 
-SELECT
-    *
-FROM
-    viewa2;
+SELECT * FROM viewa2;
